37a6037d9c0332fdbc76343b782291d4143adab8
CryptoDarkhorse <c.knight8817@gmail.com>
2022-08-13 00:45:34 +0800

    update test token address

--- config/web3.php
+++ config/web3.php
@@ -14,7 +14,7 @@ return [
     'chain' => [
       'network' => env('NETWORK', 'Rinkeby'),
       'rpc' => env('RPC_URL', 'https://eth-rinkeby.alchemyapi.io/v2/bVdkpTETSD4e0kKvvUX4Yqwtr787tkgN'),
-      'nft' => env('NFT_ADDRESS', '0xf5f9d0d0c28446c49e4be464edb76a0509336076'),
+      'nft' => env('NFT_ADDRESS', '0x968bfda2b4d206dc6b83011c9ebcc017169a6fd5'),
       'token' => env('TOKEN_ADDRESS', '0xc6fDe3FD2Cc2b173aEC24cc3f267cb3Cd78a26B7') // Yeenus test token
     ],
 
