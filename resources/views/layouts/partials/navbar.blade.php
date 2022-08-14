6c49033d1b273d66141dbf4770ee3c9186637e59
Onrio <a.silagadze96@gmail.com>
2022-08-14 04:32:21 +0400

    modal
    modal

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -4,100 +4,14 @@
       <div class="MessageToast_content__EQ4ho">Please use Brave, Chrome, or Firefox to play games</div>
     </div>
   </div>
-  <div class="ui MenuTop_menu_container__9Gy3x menu">
-    <img class="MenuTop_menu_logo__daNZS" alt="HIDENSEEK GAMES LOGO" src="{{ asset('img/logo-dark.png') }}">
-    <div class="MenuTop_menu_items_to_hide__lVnbH">
-      <section class="MenuTop_menu_item_single__syh2m">
-        <a class="item MenuTop_menu_style__G0OWe">ICE Poker</a>
-        <div class="MenuTop_navigation_submenu__w0TS0" style="width: 500px;">
-          <section class="MenuTop_grid__gG_Rx">
-            <div class="MenuTop_grid_component__26gka">
-              <a href="/ice">
-                <img src="https://res.cloudinary.com/dnzambf4m/image/upload/v1645180391/Home_Icon_fxnyp8.png" alt="dashboard">
-                <div class="MenuTop_submenu_description__7tF7g">
-                  <h1 class="MenuTop_title__9gFvx">ICE Dashboard</h1>
-                  <p>Manage all things ICE</p>
-                </div>
-              </a>
-              <a href="/ice/marketplace">
-                <img src="https://res.cloudinary.com/dnzambf4m/image/upload/v1645180391/image_65_hzpbvu.png" alt="marketplace">
-                <div class="MenuTop_submenu_description__7tF7g">
-                  <h1 class="MenuTop_title__9gFvx">Marketplace</h1>
-                  <p>Explore our offerings</p>
-                </div>
-              </a>
-            </div>
-            <div class="MenuTop_grid_component__26gka">
-              <a href="/account/items">
-                <img src="https://res.cloudinary.com/dnzambf4m/image/upload/v1645180391/image_63_rm0hba.png" alt="my items">
-                <div class="MenuTop_submenu_description__7tF7g">
-                  <h1 class="MenuTop_title__9gFvx">My Items</h1>
-                  <p>See your wallet's items</p>
-                </div>
-              </a>
-              <a href="/ice/start">
-                <img src="https://res.cloudinary.com/dnzambf4m/image/upload/v1645180391/image_62_nmwv3c.png" alt="my items">
-                <div class="MenuTop_submenu_description__7tF7g">
-                  <h1 class="MenuTop_title__9gFvx">Get Started</h1>
-                  <p>For brand new users</p>
-                </div>
-              </a>
-            </div>
-          </section>
-        </div>
-      </section>
-      <section class="MenuTop_menu_item_single__syh2m">
-        <a class="item MenuTop_menu_style__G0OWe">DAO</a>
-        <div class="MenuTop_navigation_submenu__w0TS0" style="width: 550px;">
-          <section class="MenuTop_grid__gG_Rx">
-            <div class="MenuTop_grid_component__26gka">
-              <a href="/dg">
-                <img src="https://res.cloudinary.com/dnzambf4m/image/upload/v1645180391/Home_Icon_1_n50ko4.png" alt="DAO">
-                <div class="MenuTop_submenu_description__7tF7g">
-                  <h1 class="MenuTop_title__9gFvx">DAO Dashboard</h1>
-                  <p>Overview the DAO</p>
-                </div>
-              </a>
-              <a href="/dg/treasury">
-                <img src="https://res.cloudinary.com/dnzambf4m/image/upload/v1645180391/image_69_v6rszv.png" alt="treasury">
-                <div class="MenuTop_submenu_description__7tF7g">
-                  <h1 class="MenuTop_title__9gFvx">Treasury</h1>
-                  <p>Explore our assets</p>
-                </div>
-              </a>
-            </div>
-            <div class="MenuTop_grid_component__26gka">
-              <a href="/dg/governance">
-                <img src="https://res.cloudinary.com/dnzambf4m/image/upload/v1645180391/image_63_1_ypath5.png" alt="staking">
-                <div class="MenuTop_submenu_description__7tF7g">
-                  <h1 class="MenuTop_title__9gFvx">Governance Staking</h1>
-                  <p>Stake your DG for xDG</p>
-                </div>
-              </a>
-              <a href="https://snapshot.org/#/decentralgames.eth" target="_blank">
-                <img src="https://res.cloudinary.com/dnzambf4m/image/upload/v1645180391/image_66_egowyq.png" alt="proposals">
-                <div class="MenuTop_submenu_description__7tF7g">
-                  <h1 class="MenuTop_title__9gFvx">Proposals</h1>
-                  <p>Vote on proposals in snapshot</p>
-                </div>
-              </a>
-            </div>
-          </section>
-        </div>
-      </section>
-      <a class="item MenuTop_menu_style__G0OWe">Shop</a>
-      <a class="item MenuTop_menu_style__G0OWe">Events</a>
-      <a class="item MenuTop_menu_style__G0OWe">News &amp; Blog</a>
-      <a href="https://docs.decentral.games/" id="docs-top-menu" class="d-flex" target="_blank" rel="noreferrer">
-        <div class="item MenuTop_menu_style__G0OWe">Docs</div>
-      </a>
+  <header class="ui MenuTop_menu_container__9Gy3x menu dashboard-header">
+    <div class="dashboard-logo">
+      <img alt="HIDENSEEK GAMES LOGO" src="{{ asset('img/logo-dark.png') }}">
     </div>
-    <span class="MenuTop_right_menu_items__18kaR">
-      <div class="ButtonConnect_main_right_panel__sS_5X">
-        <a href="https://metamask.io/faqs/" target="_blank" class="ButtonConnect_get_metamask__akVlL" rel="noreferrer">?</a>
-      </div>
-    </span>
-  </div>
+    <div class="">
+
+    </div>
+  </header>
   <div class="MenuTop_mobile_menu__hgvNq">
     <span class="d-flex flex-column w-100"></span>
   </div>
