b5fdc14410731c1a9b4fb2c538cc22b57cd76874
doctor-nasty <99137202+doctor-nasty@users.noreply.github.com>
2022-08-15 22:16:26 +0400

    login update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -71,47 +71,7 @@
     </head>
 
     <body>
-      <form method="POST" action="{{ route('login') }}">
-        @csrf
-      <div class="form-group">
-        <label style="color:white">@lang('login.username') *</label>
-                    <input id="text" type="username" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
 
-                    @if ($errors->has('username'))
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $errors->first('username') }}</strong>
-                        </span>
-                    @endif
-      </div>
-      <div class="form-group">
-        <label style="color:white">@lang('login.password') *</label>
-                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                    @if ($errors->has('password'))
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                    @endif
-      </div>
-      <div class="form-group d-flex align-items-center justify-content-between">
-        <div class="form-check">
-          <label style="color:white" class="white-text form-check-label">
-            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}> @lang('login.remember_me')</label>
-        </div>
-       @if (Route::has('password.request'))
-       <a href="{{ route('password.request') }}" style="color:white" class="forgot-pass">@lang('login.forgot_password')</a>
-       @endif
-      </div>
-      <div class="text-center">
-        <button type="submit" class="btn btn-primary btn-block btn-dark">@lang('login.login')</button>
-      </div>
-      {{--<div class="d-flex">--}}
-        {{--<button class="btn btn-facebook mr-2 col">--}}
-          {{--<i class="mdi mdi-facebook"></i> Facebook </button>--}}
-        {{--<button class="btn btn-google col">--}}
-          {{--<i class="mdi mdi-google-plus"></i> Google plus </button>--}}
-      {{--</div>--}}
-    </form>
 <div class="container">
 <div id="myModal" id="darkModalForm" class="modal fade" role="dialog">
     <div class="modal-dialog">
