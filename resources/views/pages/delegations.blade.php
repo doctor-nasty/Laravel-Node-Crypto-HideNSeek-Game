ca782ac3784eaa1d36edcd42b1dc48212d94c02d
Onrio <a.silagadze96@gmail.com>
2022-08-23 00:16:52 +0400

    slider fix

--- resources/views/pages/delegations.blade.php
+++ resources/views/pages/delegations.blade.php
@@ -79,7 +79,6 @@
       var swiper = new Swiper(".mySwiper", {
         slidesPerView: 3,
         spaceBetween: 30,
-        loop: true,
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
